2401455927eafb22f932f3990516ca792b053f2d
detectivelyw <detectivelyw@gmail.com>
2019-07-19 15:25:18 -0400

    update script.

--- infrastructure/kill_linuxkit.sh
+++ infrastructure/kill_linuxkit.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 cd ~/Documents/projects/linuxkit-v0.2-src/
 echo "start running the LinuxKit VM ..."
-sudo linuxkit run nginx-test &
+sudo linuxkit run nginx-test > ./logs/boot.log &
 sleep 40
 sudo kill -9 $(ps aux | grep "[q]emu" | awk {'print $2'} | head -n 1)
 sudo kill -9 $(ps aux | grep "[q]emu" | awk {'print $2'} | head -n 1)
+sed -i '$d' ./logs/boot.log
 echo "program has finished."

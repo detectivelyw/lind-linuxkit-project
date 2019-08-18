e93f2b444841b78530d146b25aa4eaf9f3ce1945
Yiwen Li <detectivelyw@gmail.com>
2019-08-18 15:16:12 -0400

    updated scripts.

--- kill_linuxkit.sh
+++ kill_linuxkit.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-cd ~/Documents/projects/linuxkit-v0.2-src/
+# usage: ./kill_linuxkit.sh [container_name]
+#example: ./kill_linuxkit.sh nginx
+
+cd $KITSRC
 echo "start running the LinuxKit VM ..."
-sudo linuxkit run nginx-test > ./logs/boot.log &
-sleep 40
+sudo linuxkit run $1 > ./logs/boot.log &
+sleep 100
 sudo kill -9 $(ps aux | grep "[q]emu" | awk {'print $2'} | head -n 1)
 sudo kill -9 $(ps aux | grep "[q]emu" | awk {'print $2'} | head -n 1)
 sed -i '$d' ./logs/boot.log
---
+++ train_pp.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# usage: ./train_pp.sh [container_name] [iteration_start_#] [iteration_end_#]
+# example" ./train_pp.sh nginx 5 100
+
+# For the first iteration, make sure that we have the most updated kernel source compiled and used 
+cd $TOOL
+./recompile-kernel.sh
+cd $KITSRC
+sudo linuxkit build -disable-content-trust $1.yml
+
+# Now start our iteration
+for i in $(seq $2 $3) 
+do
+  echo "start running training iteration $(($i-$2+1))/$(($3-$2+1)) ..."
+  cd $TOOL
+  ./kill_linuxkit.sh $1
+  cd $TOOL
+  sudo mv $KITSRC/logs/boot.log ./boot-log/
+  sudo python process-boot-log.py
+  sudo python process-blacklist.py
+  sudo mv ./boot-log/boot.log ./boot-log-processed/boot.log.$i
+  sudo mv ./blacklist/blacklist.txt ./blacklist-processed/blacklist.txt.$i
+  ./recompile-kernel.sh
+  cd $KITSRC
+  sudo linuxkit build -disable-content-trust $1.yml
+  echo "iteration $(($i-$2+1))/$(($3-$2+1)) has finished."
+done
+echo "training job has finished!"
